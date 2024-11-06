2ce8c24f4881ebbfd2543b1fc50c79bf94ed7c5e
Dmitry Kuznetsov <dimakuznetcov163@yandex.ru>
2024-11-06 12:56:49 +0400

    реализация спринт 16

--- server/src/test/java/ru.practicum.shareit/booking/BookingControllerTest.java
+++ server/src/test/java/ru.practicum.shareit/booking/BookingControllerTest.java
@@ -1,7 +1,7 @@
-package ru.practicum.shareit;
+package ru.practicum.shareit.booking;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
--- server/src/test/java/user/UserServiceImplTest.java
+++ server/src/test/java/user/UserServiceImplTest.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit;
+package user;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
